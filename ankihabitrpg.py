eb9d184c4ba9b34a83e0a42c2f48ae0a6f3617f9
Pherr <pherr@pherr.net>
2013-05-24 13:51:37 -0600

    Add ui for setup

--- ankihabitrpg.py
+++ ankihabitrpg.py
@@ -1,29 +1,53 @@
-user_id = "Your HabitRPG user id here"
-api_token = "Your HabitRPG api token here"
-
-##########################################
-import urllib2, urllib
+import urllib2, urllib,  os, sys, json
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
-from aqt.utils import showInfo
 from anki.sync import Syncer
+from aqt import *
 
-def card_answered(self, ease):
-    #Cache number of correct answers
-    if ease > 1:
-        Syncer.correct_answers += 1
-        #showInfo(str(Syncer.correct_answers))#For debugging
 
-def habit_sync(x):
-    #Call API once for every correct answer during Ankiweb sync
-    while Syncer.correct_answers > 0:
-        urllib2.urlopen(url,urllib.urlencode(headers))
-        Syncer.correct_answers -= 1
-       #showInfo("Called API. " + str(Syncer.correct_answers) + " Times remaining" )#For debugging
+def card_answered(self, ease):  # Cache number of correct answers
+    if Syncer.active == True:
+        if ease > 1:
+            Syncer.correct_answers += 1
 
+def habit_sync(x):              # Call API once for every correct answer during Ankiweb sync
+    if Syncer.active == True:
+        while Syncer.correct_answers > 0:
+            urllib2.urlopen(Syncer.url,urllib.urlencode(Syncer.headers))
+            Syncer.correct_answers -= 1
+
+def setup():
+    user_id, ok = utils.getText("Enter your user ID:")
+    if ok == True:
+        api_token, ok = utils.getText('Enter your API token:')
+        if ok == True:          # Create config file and save values
+            api_token = str(api_token)
+            user_id = str(user_id)
+            config = {'token' : api_token, 'user' : user_id}
+            json.dump( config, open( conffile, 'w' ) )
+            Syncer.active = True
+            Syncer.url = 'https://habitrpg.com/v1/users/%s/tasks/Anki/up' % user_id
+            Syncer.headers = {"apiToken":api_token}
+            utils.showInfo("The add-on has been setup.")
+
+conffile = os.path.join(os.path.dirname(os.path.realpath(__file__)), ".habitrpg.conf")
+conffile = conffile.decode(sys.getfilesystemencoding())
+Syncer.active = False
 Syncer.correct_answers = 0
-url = 'https://habitrpg.com/v1/users/' + user_id + '/tasks/Anki/up'
-headers = {"apiToken":api_token,"title":"Anki"}
+
+if os.path.exists(conffile):    # Load config file
+    config = json.load(open(conffile, 'r'))
+    api_token = config['token']
+    user_id = config['user']
+    Syncer.url = 'https://habitrpg.com/v1/users/%s/tasks/Anki/up' % user_id
+    Syncer.headers = {"apiToken":api_token}
+    Syncer.active = True
+
+
+#Add Setup to menubar
+action = QAction("Setup HabitRPG", mw)
+mw.connect(action, SIGNAL("triggered()"), setup)
+mw.form.menuTools.addAction(action)
 
 #Wrap funtions to Anki
 Reviewer._answerCard = wrap(Reviewer._answerCard, card_answered)
